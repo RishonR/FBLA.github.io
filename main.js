3a77b1ffef5113ee235cfeef2d50f72572468676
balls321 <163483492+balls321@users.noreply.github.com>
2024-04-05 11:53:26 -0500

    index.html

--- main.js
+++ main.js
@@ -32,7 +32,6 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     });
-    
 
     // Search functionality
     document.getElementById('search').addEventListener('input', function() {
